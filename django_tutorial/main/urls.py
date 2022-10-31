cc97de68438d40935a899c4df8fa7b391f9b6fd1
Tyler-Balka <115902729+Tyler-Balka@users.noreply.github.com>
2022-10-31 10:06:06 -0500

    Delete django_tutorial directory

--- django_tutorial/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views # . references current directory
-
-urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('home', views.home, name='home'),
-    path('sign-up', views.sign_up, name = 'sign_up'),
-    path('add_emp', views.add_emp, name='add_emp')
-]
